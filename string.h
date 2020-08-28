abc729e0befcaf57b8a895066101ae930231290e
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-28 19:56:40 +0100

    string lib

--- string.h
+++ string.h
@@ -10,7 +10,7 @@ int incmp(char *s,char *s2);
 int strsfills(char *s,char c);
 void strsint(char *s,int n);
 void strslong(char *s,long n);
-
+void strsfloat(char *s,float n);
 
 int strslen(char *s){
 	int pos=0;
@@ -158,4 +158,36 @@ void strslong(char *s,long n){
 	}
 	s[pos+1]=0;
 }
+
+void strsfloat(char *s,float n){
+	float nn=n;
+	float nnn=n;
+	char pp=0;
+	int pos=0;
+	int pos2=1;
+	float signals=n;
+	float divsb=100.00f;
+	float divsa=0;
+	s[pos]='+';
+	if(signals<0){
+		s[pos]='-';
+		nn=0-nn;
+	}
+	for(pos=0;pos<5;pos++){
+		nnn=nn;
+		divsa=nn/divsb;
+		if(pos==3){
+			pos2++;
+			s[pos+1]='.';
+		}
+		pp=(char) divsa;
+		nn=nnn-(divsb*pp);
+		pp=pp+'0';
+		s[pos+pos2]=(char) pp;
+		divsb=divsb/10.00f;
+	}
+	s[pos+pos2]=0;
+}
+
+
 
